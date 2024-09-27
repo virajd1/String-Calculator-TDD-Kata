511acd0f36695e78ed3c947b437f583d66f77442
virajd1 <viraj.d@sankeysolutions.com>
2024-09-27 22:21:40 +0530

    delimiter test cases added

--- test/StringCalculator.test.ts
+++ test/StringCalculator.test.ts
@@ -24,5 +24,16 @@ describe('String Calculator', () => {
     it('should return sum when input is two numbers', () => {
         expect(calculator.add('1,2')).to.equal(3);
     });
+    it('should use the delimiter when first line contains a delimiter', () => {
+        expect(calculator.add('#|\n12|5\n1')).to.equal(18);
+    });
+
+    it('should support different delimiters when first line declares the delimiter started with hash:', () => {
+        expect(calculator.add('#|\n1 | 2 | 10')).to.equal(13);
+    });
+
+    it('should support different delimiters with any length', () => {
+        expect(calculator.add('#--\n1--7--23\n4--4')).to.equal(39);
+    });
 
 });
