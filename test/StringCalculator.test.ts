75649dc0b80fe5652457b3cfb1b286c529ac2c7d
virajd1 <viraj.d@sankeysolutions.com>
2024-09-27 22:06:50 +0530

    basic test cases added

--- test/StringCalculator.test.ts
+++ test/StringCalculator.test.ts
@@ -5,6 +5,24 @@ import 'mocha';
 
 describe('String Calculator', () => {
     let calculator: any;
+    beforeEach(() => {
+        calculator = new StringCalculator;
+    })
 
+    it('should return zero when input is null', () => {
+        expect(calculator.add(null)).to.equal(0);
+    });
+
+    it('should return zero when input is empty', () => {
+        expect(calculator.add('')).to.equal(0);
+    });
+
+    it('should return the number when input is a number', () => {
+        expect(calculator.add('1')).to.equal(1);
+    });
+
+    it('should return sum when input is two numbers', () => {
+        expect(calculator.add('1,2')).to.equal(3);
+    });
 
 });
