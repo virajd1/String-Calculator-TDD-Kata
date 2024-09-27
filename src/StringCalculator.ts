8297dcce01b7a25e7711211af78892c11d910b19
virajd1 <viraj.d@sankeysolutions.com>
2024-09-27 22:00:10 +0530

    string cal function

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,4 +1,12 @@
 export class StringCalculator {
+    add(str: any) {
+        let reducer = (accumulator: number, currentValue: number) => accumulator + currentValue;
 
+        if (str !== null) {
+            return str.replace(/[^0-9\.]+/g, ' ').split(' ').map(Number).reduce(reducer);
+        } else {
+            return 0;
+        }
+    }
 
 }
